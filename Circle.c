f7c3052985c355f8ed3e273595f3ed32a5394fad
abdelrahmanMohamed <abdo.mohamed.am.ao@gmail.com>
2017-10-12 01:22:52 +0300

    Update Circle.c

--- Circle.c
+++ Circle.c
@@ -32,14 +32,14 @@ int main(void)
 	// Make the window's context current
 	glfwMakeContextCurrent(window);
 
-	glViewport(0.0f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT); // specifies the part of the window to which OpenGL will draw (in pixels), convert from normalised to pixels
-	glMatrixMode(GL_PROJECTION); // projection matrix defines the properties of the camera that views the objects in the world coordinate frame. Here you typically set the zoom factor, aspect ratio and the near and far clipping planes
-	glLoadIdentity(); // replace the current matrix with the identity matrix and starts us a fresh because matrix transforms such as glOrpho and glRotate cumulate, basically puts us at (0, 0, 0)
-	glOrtho(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 1); // essentially set coordinate system
-	glMatrixMode(GL_MODELVIEW); // (default matrix mode) modelview matrix defines how your objects are transformed (meaning translation, rotation and scaling) in your world
-	glLoadIdentity(); // same as above comment
+	glViewport(0.0f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT); 
+	glMatrixMode(GL_PROJECTION);  
+	glLoadIdentity(); 
+	glOrtho(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 1);
+	glMatrixMode(GL_MODELVIEW); 
+	glLoadIdentity(); 
 
-					  // Loop until the user closes the window
+					 
 	while (!glfwWindowShouldClose(window))
 	{
 		processInput(window);
